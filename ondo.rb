b8942af02a6e75608872a8f735fa5b7756dbb3d1
suisho <suisho000000@gmail.com>
2014-07-12 17:40:48 +0900

    init

--- ondo.rb
+++ ondo.rb
@@ -1,18 +1,16 @@
 require 'pi_piper'
 require 'firebase'
-base_uri = 'https://torid-fire-7950.firebaseio.com/'
+base_uri = 'https://torid-fire-7950.firebaseio.com/list'
 
 firebase = Firebase::Client.new(base_uri)
 
-loop do
-  value = 0
-  PiPiper::Spi.begin do |spi|
-    raw = spi.write [0b01101000,0]
-    value = ((raw[0]<<8) + raw[1]) & 0x03FF
-  end
-  volt = (value * 3300)/1024
-  degree = (volt - 500)/10
-  puts degree
-  firebase.push({degree: degree, time: Time.now})
-  sleep(1)
+value = 0
+PiPiper::Spi.begin do |spi|
+  raw = spi.write [0b01101000,0]
+  value = ((raw[0]<<8) + raw[1]) & 0x03FF
 end
+volt = (value * 3300)/1024
+degree = (volt - 500)/10
+puts degree
+response = firebase.push("degree", {degree: degree, time: Time.now})
+response = firebase.set("last-degree", degree)
