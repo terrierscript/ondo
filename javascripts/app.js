e91ca401b2a65e2e2ed82964377728ed56f827da
suisho <suisho000000@gmail.com>
2014-07-24 22:57:15 +0900

    fix for opera

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,36 @@
-var Degrees = new Firebase("https://torid-fire-7950.firebaseio.com/degree_log");
-Degrees.limit(60).on("value", function(d){
-  app.loading = false
-  app.logs = []
-  d.forEach(function(log){
-    app.logs.unshift(log.val())
-  })
-})
-
-var app = new Vue({
-  el : "#console",
-  data : {
-    loading: true,
-    logs:[]
-  },
-  filters : {
-    timeago : function(timeStr){
-      var sec = ((new Date() - new Date(timeStr))/1000)
-      var min = sec/60
-      return Math.ceil(min) + " min ago"
+(function(){
+  Vue.directive("textcontent", {
+    bind : function(){
+      this.vm.$set(this.key, this.el.textContent)
     }
-  }
-})
+  })
+  var Degrees = new Firebase("https://torid-fire-7950.firebaseio.com/degree_log");
+  Degrees.limit(60).on("value", function(d){
+    app.loading = false
+    app.logs = []
+    d.forEach(function(log){
+      app.logs.unshift(log.val())
+    })
+  })
 
-
+  var app = new Vue({
+    el : "#console",
+    data : {
+      loading: true,
+      logs:[]
+    },
+    filters : {
+      timeago : function(timeStr){
+        timeStr = timeStr.replace(/-/g, "/") // for opera
+        var sec = ((new Date() - new Date(timeStr))/1000)
+        var min = sec/60
+        return Math.ceil(min) + " min ago"
+      }
+    }
+  })
+  var foo = new Vue({
+    el : "#foo",
+  })
+  console.log(foo.baz)
+  foo.$data.baz = "bee"
+})()
