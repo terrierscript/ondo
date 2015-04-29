69ec6d977a9b94bc0de9b3e493e61995a973069e
suisho <suisho000000@gmail.com>
2015-04-29 16:37:20 +0900

    add ondy

---
+++ arduino/ondo.js
@@ -0,0 +1,20 @@
+var five = require("johnny-five")
+var board = new five.Board()
+
+module.exports = function(cb){
+  board.once("ready", function() {
+    // This requires OneWire support using the ConfigurableFirmata
+    var temperature = new five.Temperature({
+      controller: "LM35",
+      pin: "A0"
+    })
+
+    temperature.once("data", function(err, data){
+      if(err){
+        console.log(err)
+        return
+      }
+      cb(err, data.celsius)
+    })
+  })
+}
